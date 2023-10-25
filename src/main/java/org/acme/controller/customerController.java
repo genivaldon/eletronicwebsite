d4cdd347238398c4587c14c6e4fa227066c8d2de
genivaldon <genivaldo.honorato@dcx.ufpb.br>
2023-10-25 20:02:43 -0300

    implementacao do microservico customer

---
+++ src/main/java/org/acme/controller/customerController.java
@@ -0,0 +1,74 @@
+package org.acme.controller;
+
+import java.util.List;
+
+import org.acme.dto.CustomerDTO;
+import org.acme.service.CustomerService;
+
+
+
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+
+@Path("/api/customers")
+public class customerController {
+    
+    @Inject
+    CustomerService customerService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<CustomerDTO> findAllCustomers(){
+        return customerService.findAllCustomers();
+    }
+
+    @POST
+    @Transactional
+    public Response saveCustomer(CustomerDTO customerDTO){
+        try{
+            customerService.createNewCostumer(customerDTO);
+            return Response.ok().build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.serverError().build();
+        }
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Transactional
+    public Response changeCustomer(@PathParam("id") long id, CustomerDTO custumerDTO){
+
+        try {
+            customerService.changeCustomer(id, custumerDTO);
+            return Response.ok().build();
+        } catch (Exception e){
+            e.printStackTrace();
+            return Response.serverError().build();
+        }
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public Response DeleteCustomer (@PathParam("id") long id){
+
+        try{
+            customerService.deleteCustomer(id);
+            return Response.ok().build();
+        } catch (Exception e) {
+            return Response.serverError().build();
+
+        }
+}
+}
---
+++ src/main/java/org/acme/service/CustomerService.java
@@ -0,0 +1,75 @@
+package org.acme.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.acme.dto.CustomerDTO;
+import org.acme.entity.CustomerEntity;
+import org.acme.repository.CustomerRepository;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+@ApplicationScoped
+public class CustomerService {
+    
+    @Inject
+    private CustomerRepository customerRepository;
+
+    public List<CustomerDTO> findAllCustomers(){
+        List<CustomerDTO> customers = new ArrayList<>();
+        customerRepository.findAll().stream().forEach(item->{
+            customers.add(mapCustomerEntityToDTO(item));
+        });
+
+        return customers;
+    }
+
+    public void createNewCostumer(CustomerDTO customer){
+        customerRepository.persist(mapCustomerDTOToEntity(customer));
+
+    }
+
+    public void changeCustomer(Long id, CustomerDTO customerDTO){
+        CustomerEntity customer = customerRepository.findById(id);
+
+        customer.setName(customerDTO.getName());
+        customer.setAddress(customerDTO.getAddress());
+        customer.setAge(customerDTO.getAge());
+        customer.setEmail(customerDTO.getEmail());
+        customer.setPhone(customerDTO.getPhone());
+
+        customerRepository.persist(customer);
+
+    }
+
+    public void deleteCustomer (long id){
+        customerRepository.deleteById(id);
+    }
+
+    private CustomerDTO mapCustomerEntityToDTO(CustomerEntity customer){
+
+        CustomerDTO customerDTO = new CustomerDTO();
+
+        customerDTO.setAddress(customer.getAddress());
+        customerDTO.setAge(customer.getAge());
+        customerDTO.setEmail(customer.getEmail());
+        customerDTO.setName(customer.getName());
+        customerDTO.setPhone(customer.getPhone());
+
+        return customerDTO;
+    }
+
+        private CustomerEntity mapCustomerDTOToEntity(CustomerDTO customer){
+
+        CustomerEntity customerEntity = new CustomerEntity();
+
+        customerEntity.setAddress(customer.getAddress());
+        customerEntity.setAge(customer.getAge());
+        customerEntity.setEmail(customer.getEmail());
+        customerEntity.setName(customer.getName());
+        customerEntity.setPhone(customer.getPhone());
+
+        return customerEntity;
+    }
+}
